cda474230bbcf77b0729b1b8f5225654f4f3375a
Esail <yguo0606@gmail.com>
2019-03-29 21:31:08 +0800

    update

--- src/main/scala/spark/basic/TextDemo.scala
+++ src/main/scala/spark/basic/TextDemo.scala
@@ -3,7 +3,10 @@ package spark.basic
 import org.apache.spark.sql.SparkSession
 
 object TextDemo extends App {
-	val spark = SparkSession.builder().master("local[*]").getOrCreate()
+	val spark = SparkSession.builder()
+		.master("local[*]")
+		.config("spark.sql.warehouse.dir", "/tmp/spark-warehouse")
+		.getOrCreate()
 	val words = this.getClass.getResource("/words.txt").getPath
 	val sc = spark.sparkContext
 	import spark.implicits._
